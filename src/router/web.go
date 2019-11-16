9c31fb4c9dc51f8a9b79ab7153dfcb3457670088
roberto.ferro <roberto.ferro@ikdev.eu>
2019-11-16 12:12:45 +0100

    Insert test middleware and renamed controller file

--- src/router/web.go
+++ src/router/web.go
@@ -3,11 +3,13 @@ package router
 import (
 	"github.com/gorilla/mux"
 	"smartcharry/src/controller"
+	"smartcharry/src/middleware"
 )
 
 func WebRouter() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/", controller.TestHandler)
+	router.Use(middleware.LoggingMiddleware)
 
 	return router
 }
