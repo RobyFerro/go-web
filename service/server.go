a8423b4debbb4e1517bdba8ab4334592e174e141
roberto.ferro <roberto.ferro@ikdev.eu>
2019-12-03 19:18:48 +0100

    Removed unused properties

--- service/server.go
+++ service/server.go
@@ -11,18 +11,7 @@ func GetHttpServer(router *mux.Router, cfg config.Conf) *http.Server {
 	serverString := fmt.Sprintf(":%d", cfg.Server.Port)
 
 	return &http.Server{
-		Addr:              serverString,
-		Handler:           router,
-		TLSConfig:         nil,
-		ReadTimeout:       0,
-		ReadHeaderTimeout: 0,
-		WriteTimeout:      0,
-		IdleTimeout:       0,
-		MaxHeaderBytes:    0,
-		TLSNextProto:      nil,
-		ConnState:         nil,
-		ErrorLog:          nil,
-		BaseContext:       nil,
-		ConnContext:       nil,
+		Addr:    serverString,
+		Handler: router,
 	}
 }
