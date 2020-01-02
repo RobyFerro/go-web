a5eec72df80e713d745b4264fd072396a6cceefc
Roberto Ferro <roberto.ferro@verticalbooking.com>
2020-01-02 12:48:10 +0100

    Improve actions

--- router/action.go
+++ router/action.go
@@ -5,47 +5,54 @@ import (
 	"net/http"
 )
 
+type Action struct {
+	Controller controller.Controller
+}
+
+func NewAction(w http.ResponseWriter, r *http.Request) *Action {
+	action := new(Action)
+	action.Controller = controller.Controller{
+		DB:       db,
+		Response: w,
+		Request:  r,
+		Config:   conf,
+	}
+
+	return action
+}
+
 func homeAction(w http.ResponseWriter, r *http.Request) {
+	action := NewAction(w, r)
 	homeController := controller.HomeController{
-		Controller: controller.Controller{
-			DB:       db,
-			Response: w,
-			Request:  r,
-			Config:   conf,
-		}}
+		Controller: action.Controller,
+	}
 
 	homeController.Show()
 }
 
 func newUserAction(w http.ResponseWriter, r *http.Request) {
-	userController := controller.UserController{Controller: controller.Controller{
-		DB:       db,
-		Response: w,
-		Request:  r,
-		Config:   conf,
-	}}
+	action := NewAction(w, r)
+	userController := controller.UserController{
+		Controller: action.Controller,
+	}
 
 	userController.Insert()
 }
 
 func testUserAction(w http.ResponseWriter, r *http.Request) {
-	userController := controller.UserController{Controller: controller.Controller{
-		DB:       db,
-		Response: w,
-		Request:  r,
-		Config:   conf,
-	}}
+	action := NewAction(w, r)
+	userController := controller.UserController{
+		Controller: action.Controller,
+	}
 
 	userController.Profile()
 }
 
 func loginAction(w http.ResponseWriter, r *http.Request) {
-	authController := controller.AuthController{Controller: controller.Controller{
-		DB:       db,
-		Response: w,
-		Request:  r,
-		Config:   conf,
-	}}
+	action := NewAction(w, r)
+	authController := controller.AuthController{
+		Controller: action.Controller,
+	}
 
 	authController.Login()
 }
