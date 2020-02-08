455629f050a9d3432d31329ebb60b96076196a6a
roberto.ferro <roberto.ferro@ikdev.eu>
2020-02-08 11:43:22 +0100

    Improve controller parser

--- app/console/kernel.go
+++ app/console/kernel.go
@@ -2,10 +2,10 @@ package console
 
 import "github.com/RobyFerro/go-web/app/console/command"
 
-// This method will return an array of commands.
-// Used by Go-Web routing
-// Every time you add a new controller you should register it in this method
-
+// Register all commands here.
+// The following map of interfaces expose all available method that can be used by Go-Web CLI tool.
+// The map index determines the command that you've to run to for use the relative method.
+// Example: './goweb migration:up' will run '&command.MigrationUp{}' command.
 var (
 	Commands = map[string]interface{}{
 		"migration:up":       &command.MigrationUp{},
